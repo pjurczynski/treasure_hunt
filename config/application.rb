b18d48cde04ecf7770a84895fd4a188ee1e1dfcb
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2016-10-02 01:26:28 +0200

    autoload serializers

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -9,8 +10,6 @@ Bundler.require(*Rails.groups)
 
 module TreasureHunt
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths << Rails.root.join('app/serializers')
   end
 end
