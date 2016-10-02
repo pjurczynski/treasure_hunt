2af8785ea94767566c36c184b3b865eee5a80883
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2016-10-02 16:13:03 +0200

    add winning scope

--- app/models/hunt.rb
+++ app/models/hunt.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 class Hunt < ApplicationRecord
+  scope :winning_locations, (lambda do |location|
+    WinningLocationsQuery.new(
+      relation: self,
+      column: :current_location,
+      point: String(location),
+    ).call
+  end)
 end
--- spec/models/hunt_spec.rb
+++ spec/models/hunt_spec.rb
@@ -2,4 +2,23 @@
 require 'rails_helper'
 
 describe Hunt do
+  describe '.wininng_locations' do
+    it 'calls query' do
+      query_class = WinningLocationsQuery
+      query_instance = double('query')
+      my_point = Utils.point(longitude: 1, latitude: 1)
+
+      aggregate_failures do
+        expect(query_class).to receive(:new).with(
+          relation: Hunt,
+          column: :current_location,
+          point: my_point,
+        ).and_return(query_instance)
+
+        expect(query_instance).to receive(:call)
+      end
+
+      described_class.winning_locations(my_point)
+    end
+  end
 end
