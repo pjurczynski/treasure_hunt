7d7730141e49dc4fc706a3e14d563e665f35de88
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2016-10-02 16:17:16 +0200

    notify the treasure hunter about winning through email

--- app/operations/treasure_hunt/create.rb
+++ app/operations/treasure_hunt/create.rb
@@ -12,6 +12,14 @@ class TreasureHunt::Create < ApplicationOperation
     validates :email, presence: true
     validates :current_location, presence: true
     validates :treasure_location, presence: true
+
+    def won?
+      distance <= Treasure::WINNING_RADIUS
+    end
+
+    def already_won?
+      Hunt.where(email: email).winning_locations(treasure_location).exists?
+    end
   end
 
   contract(Contract)
@@ -20,12 +28,18 @@ class TreasureHunt::Create < ApplicationOperation
     @model = Hunt.new
 
     validate(params, @model) do |form|
-      form.save
       form.distance = form.current_location.distance(form.treasure_location)
+
+      return if form.already_won?
+
+      form.save
+
+      notify_hunter_he_found_treasure!(form.email, form.treasure_location) if form.won?
     end
   end
 
-  def already_won?
-    Hunt.where(email: email).winning_locations(treasure_location).exist?
+  def notify_hunter_he_found_treasure!(email, treasure_location)
+    found_treasure_count = Hunt.winning_locations(treasure_location).count
+    TreasureMailer.found(email, found_treasure_count).deliver_later
   end
 end
