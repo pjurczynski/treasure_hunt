effa22a0ebd05f028536cefff4366ba8eddf5cb5
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2016-10-02 13:28:02 +0200

    save new hunt if valid

--- app/controllers/api/v1/treasure_hunts_controller.rb
+++ app/controllers/api/v1/treasure_hunts_controller.rb
@@ -3,6 +3,23 @@
 module API::V1
   class TreasureHuntsController < API::ApplicationController
     def create
+      run ::TreasureHunt::Create, params: treasure_hunt_params
+
+      if @operation.valid?
+        render json: @operation,
+               status: :created,
+               serializer: API::V1::TreasureHunt::CreateSerializer
+      else
+        render json: @operation,
+               status: :unprocessable_entity,
+               serializer: API::V1::TreasureHunt::ErrorSerializer
+      end
+    end
+
+    private
+
+    def treasure_hunt_params
+      params.merge(treasure_location: Treasure.first.try(:location))
     end
   end
 end
--- spec/controllers/api/v1/treasure_hunts_controller_spec.rb
+++ spec/controllers/api/v1/treasure_hunts_controller_spec.rb
@@ -1,12 +1,56 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 describe API::V1::TreasureHuntsController do
   describe 'POST create' do
-    context 'with location within the range of the event' do
-      subject { post :create }
+    subject { post :create, params: params }
 
-      it { is_expected.to have_http_status :ok }
+    context 'with location within the range of the event' do
+      before { create(:treasure, :cracow) }
+      let(:hunt) { build(:hunt, :cracow) }
+      let(:params) do
+        {
+          current_location: hunt.current_location.coordinates,
+          email: hunt.email,
+        }
+      end
+
+      it { is_expected.to have_http_status :created }
+      it { expect { subject }.to change(Hunt, :count) }
+
+      it 'returns expected json structure' do
+        subject
+        aggregate_failures do
+          expect(json_response[:status]).to eq 'ok'
+          expect(json_response[:distance]).to eq 0
+        end
+      end
+
+      it 'sends an email'
+    end
+
+    context 'invalid request' do
+      let(:params) { {} }
+
+      it { is_expected.to have_http_status :unprocessable_entity }
+
+      it 'returns expected json structure' do
+        subject
+        aggregate_failures do
+          expect(json_response[:status]).to eq 'error'
+          expect(json_response[:distance]).to eq(-1)
+          expect(json_response[:error])
+            .to eq [
+              "Email can't be blank",
+              "Current location can't be blank",
+              "Treasure location can't be blank",
+            ].join(', ')
+        end
+      end
+
+      it "doesn't send an email"
+      it "doesn't create a new hunt"
     end
   end
 end
