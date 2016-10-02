b0e5e7cdd43d8dd4202cb30e7ac78b36894a498c
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2016-10-02 16:18:48 +0200

    update treasure hunts controller spec

--- spec/controllers/api/v1/treasure_hunts_controller_spec.rb
+++ spec/controllers/api/v1/treasure_hunts_controller_spec.rb
@@ -2,12 +2,24 @@
 
 require 'rails_helper'
 
+shared_examples 'runs operation' do
+  it 'uses operation' do
+    subject
+    expect(operation).to have_received(:run).with(
+      controller.params.merge(treasure_location: treasure.location),
+    )
+  end
+end
+
 describe API::V1::TreasureHuntsController do
   describe 'POST create' do
+    let(:operation) { TreasureHunt::Create }
+    before { allow(operation).to receive(:run).and_call_original }
+
     subject { post :create, params: params }
 
     context 'with location within the range of the event' do
-      before { create(:treasure, :cracow) }
+      let!(:treasure) { create(:treasure, :cracow) }
       let(:hunt) { build(:hunt, :cracow) }
       let(:params) do
         {
@@ -17,7 +29,7 @@ describe API::V1::TreasureHuntsController do
       end
 
       it { is_expected.to have_http_status :created }
-      it { expect { subject }.to change(Hunt, :count) }
+      it_behaves_like 'runs operation'
 
       it 'returns expected json structure' do
         subject
@@ -27,7 +39,41 @@ describe API::V1::TreasureHuntsController do
         end
       end
 
-      it 'sends an email'
+      context 'sending location when already won' do
+        before { hunt.save }
+
+        it { is_expected.to have_http_status :ok }
+        it_behaves_like 'runs operation'
+
+        it 'returns expected json structure' do
+          subject
+          aggregate_failures do
+            expect(json_response[:status]).to eq 'ok'
+            expect(json_response[:distance]).to eq 0
+          end
+        end
+      end
+    end
+
+    context 'with location out of the range of the event' do
+      let(:treasure) { create(:treasure, :cracow) }
+      let(:params) do
+        {
+          current_location: [treasure.location.x, treasure.location.y + 0.000_06],
+          email: 'test@example.com',
+        }
+      end
+
+      it { is_expected.to have_http_status :created }
+      it_behaves_like 'runs operation'
+
+      it 'returns expected json structure' do
+        subject
+        aggregate_failures do
+          expect(json_response[:status]).to eq 'ok'
+          expect(json_response[:distance]).to eq 6
+        end
+      end
     end
 
     context 'invalid request' do
